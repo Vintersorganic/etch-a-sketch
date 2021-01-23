f5b2673390f77169d0f06a858c177107047618dd
Varulvsnatt <jorge.gdp@gmail.com>
2021-01-23 02:15:20 -0300

    New buttons for colors

--- script.js
+++ script.js
@@ -19,8 +19,12 @@ function gridReset(){
         square.style.background = 'white';
     });
 }
+
 document.getElementById("enter").addEventListener("click", gridCreation);
 document.getElementById("reset").addEventListener("click", gridReset);
+document.getElementById("black").addEventListener("click", blackColor);
+document.getElementById("randomColor").addEventListener("click", colorChange);
+
 
 function setGridSize(num){
     document.getElementById("container").style.gridTemplateColumns = `repeat(${num}, 1fr)`;
@@ -35,7 +39,6 @@ function colorChange(){
     let color = [, "#2e7248", "#3C9EE7", "#E7993C","#E73C99", "#3CE746", "#E7993C", "#903555", "#3190bf", 
     "#b59e50", "#5ec7b2", "#5c3cf6", "#ad613c", "#2fbb81", "#9b674a", "#749ccc", "#fe2a5b","#68a626"]; 
     let squares = document.querySelectorAll(".col");
-    console.log(squares)
     squares.forEach((square) => {
 
 
@@ -43,8 +46,19 @@ function colorChange(){
     square.style.background = color[Math.floor(Math.random() * color.length)];
 });
 });
-
 }
+
+function blackColor() {
+    let squares = document.querySelectorAll(".col");
+    squares.forEach((square) => {
+
+
+    square.addEventListener('mouseover', () => {
+    square.style.background = "black";
+});
+});
+}
+
 
 
   
